cac25ca7da799a268348b6e96625cd74592a1d50
Jihyeon Ha <31359128+haaajh@users.noreply.github.com>
2019-07-25 21:46:56 +0900

    Add ReadEncoder function _jihyeon

--- CouserBot_core/src/CouserBot/CouserBot_motor_driver.cpp
+++ CouserBot_core/src/CouserBot/CouserBot_motor_driver.cpp
@@ -20,7 +20,8 @@
 
 CouserBotMotorDriver::CouserBotMotorDriver()
 : baudrate_(BAUDRATE),
-  protocol_version_(PROTOCOL_VERSION)
+  protocol_version_(PROTOCOL_VERSION),
+  dcmotor_limit_max_velocity_(DCMOTOR_LIMIT_MAX_VELOCITY)
 {
 }
 
@@ -49,8 +50,8 @@ bool CouserBotMotorDriver::controlMotor(const float wheel_radius, const float wh
   wheel_velocity_cmd[LEFT]   = lin_vel - (ang_vel * wheel_separation / 2);
   wheel_velocity_cmd[RIGHT]  = lin_vel + (ang_vel * wheel_separation / 2);
 
-  wheel_velocity_cmd[LEFT]  = constrain(SCALING_FACTOR * MOTOR_VELOCITY_COEFFICIENT * wheel_velocity_cmd[LEFT] / wheel_radius, -DCMOTOR_LIMIT_MAX_VELOCITY, DCMOTOR_LIMIT_MAX_VELOCITY);
-  wheel_velocity_cmd[RIGHT] = constrain(SCALING_FACTOR * MOTOR_VELOCITY_COEFFICIENT * wheel_velocity_cmd[RIGHT] / wheel_radius, -DCMOTOR_LIMIT_MAX_VELOCITY, DCMOTOR_LIMIT_MAX_VELOCITY);
+  wheel_velocity_cmd[LEFT]  = constrain(SCALING_FACTOR * MOTOR_VELOCITY_COEFFICIENT * wheel_velocity_cmd[LEFT] / wheel_radius, -dcmotor_limit_max_velocity_, dcmotor_limit_max_velocity_);
+  wheel_velocity_cmd[RIGHT] = constrain(SCALING_FACTOR * MOTOR_VELOCITY_COEFFICIENT * wheel_velocity_cmd[RIGHT] / wheel_radius, -dcmotor_limit_max_velocity_, dcmotor_limit_max_velocity_);
 
   if(wheel_velocity_cmd[LEFT] >= 0){
     LMotor->run(Forward);
@@ -71,50 +72,24 @@ bool CouserBotMotorDriver::controlMotor(const float wheel_radius, const float wh
  return true;
 }
 
-bool CouserBotMotorDriver::readEncoder(int32_t &left_value, int32_t &right_value)
+bool CouserBotMotorDriver::readEncoder(int32_t &left_value, int32_t &right_value, int32_t &left_encoder, int32_t &right_encoder)
 {
   //Encoder 값 받아오기
   //Encoder 값 Calibration
   //sensor_state_msg에 left_encoder, right_encoder 값 넣기
 
+  int32_t scaler = 1; //-> No need
+
+  //Encoder 값 Scaling
+  left_value *= scaler;
+  right_value *= scaler;
+
+  //sensor_state_msg에 value 넣기
+  left_encoder = left_value;
+  right_encoder = right_value;
   //*** sensor_state_msg.left_encoder, sensor_state_msg.right_encoder 
   
+  //Encoder 값 검증 Code -> FAIL 시 FALSE 반환
   
-  
-  
-  /*
-  int dxl_comm_result = COMM_TX_FAIL;              // Communication result
-  bool dxl_addparam_result = false;                // addParam result
-  bool dxl_getdata_result = false;                 // GetParam result
-
-  // Set parameter
-  dxl_addparam_result = groupSyncReadEncoder_->addParam(left_wheel_id_);
-  if (dxl_addparam_result != true)
-    return false;
-
-  dxl_addparam_result = groupSyncReadEncoder_->addParam(right_wheel_id_);
-  if (dxl_addparam_result != true)
-    return false;
-
-  // Syncread present position
-  dxl_comm_result = groupSyncReadEncoder_->txRxPacket();
-  if (dxl_comm_result != COMM_SUCCESS)
-    Serial.println(packetHandler_->getTxRxResult(dxl_comm_result));
-
-  // Check if groupSyncRead data of Dynamixels are available
-  dxl_getdata_result = groupSyncReadEncoder_->isAvailable(left_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-  if (dxl_getdata_result != true)
-    return false;
-
-  dxl_getdata_result = groupSyncReadEncoder_->isAvailable(right_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-  if (dxl_getdata_result != true)
-    return false;
-
-  // Get data
-  left_value  = groupSyncReadEncoder_->getData(left_wheel_id_,  ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-  right_value = groupSyncReadEncoder_->getData(right_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-
-  groupSyncReadEncoder_->clearParam();
-  */
   return true;
 }
